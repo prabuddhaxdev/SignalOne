d0b3413bb579c9d2057ea1794d27e894a7b58a0e
prabuddhaxdev <prabuddha1749@gmail.com>
2025-11-27 19:36:41 +0530

    Implement Daily news summary

--- lib/nodemailer/index.ts
+++ lib/nodemailer/index.ts
@@ -1,5 +1,5 @@
 import nodemailer from "nodemailer";
-import { WELCOME_EMAIL_TEMPLATE } from "@/lib/nodemailer/templates";
+import { NEWS_SUMMARY_EMAIL_TEMPLATE, WELCOME_EMAIL_TEMPLATE } from "@/lib/nodemailer/templates";
 
 export const transporter = nodemailer.createTransport({
   service: "gmail",
@@ -24,6 +24,32 @@ export const sendWelcomeEmail = async ({
     to: email,
     subject: `Welcome to SignalOne - your stock market toolkit is ready!`,
     text: "Thanks for joining SignalOne",
+    html: htmlTemplate,
+  };
+
+  await transporter.sendMail(mailOptions);
+};
+
+
+export const sendNewsSummaryEmail = async ({
+  email,
+  date,
+  newsContent,
+}: {
+  email: string;
+  date: string;
+  newsContent: string;
+}): Promise<void> => {
+  const htmlTemplate = NEWS_SUMMARY_EMAIL_TEMPLATE.replace(
+    "{{date}}",
+    date
+  ).replace("{{newsContent}}", newsContent);
+
+  const mailOptions = {
+    from: `"SignalOne News" <signalone@prabuddhaxdev.in>`,
+    to: email,
+    subject: `📈 Market News Summary Today - ${date}`,
+    text: `Today's market news summary from SignalOne`,
     html: htmlTemplate,
   };
 
